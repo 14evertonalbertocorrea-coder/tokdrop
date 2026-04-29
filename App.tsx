745b70a711ea9de4b062ad6379359c7175d63904
14evertonalbertocorrea-coder <14evertonalbertocorrea@gmail.com>
2026-04-29 18:48:46 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,24 +1,18 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Terms from './pages/Terms';
 import Contact from './pages/Contact';
-import HowToDownload from './pages/HowToDownload';
-import MobileDownloader from './pages/MobileDownloader';
-import DownloadFAQ from './pages/DownloadFAQ';
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/how-to-download-tiktok-video" element={<HowToDownload />} />
-        <Route path="/tiktok-downloader-mobile" element={<MobileDownloader />} />
-        <Route path="/download-tiktok-faq" element={<DownloadFAQ />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
